c1f138e6fd12cfd3751df89a73004d7b9b0cd036
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-23 18:01:59 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,5 +2,13 @@ class Customer
   attr_accessor :name, :age 
   @@all = []
   
+  def initialize(name, age)
+    @name = name 
+    @age = age
+    @@all << self 
+  end 
   
+  def self.all
+    @@all 
+  end 
 end
